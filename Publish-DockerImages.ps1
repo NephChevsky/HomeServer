911d622af920b387c705b2a4a3d0c2cc54682f5e
Neph <philippe.balleydier@gmail.com>
2025-06-13 18:01:10 +0200

    use docker registry

--- Publish-DockerImages.ps1
+++ Publish-DockerImages.ps1
@@ -5,60 +5,31 @@ Set-Location $PSScriptRoot\WebAPI
 "Building WebAPI image"
 docker buildx build --platform linux/arm64 -t homeserver-webapi:arm --load .
 
-"Exporting WebAPI image to tar file"
-if (-not(Test-Path "bin"))
-{
-    New-Item -ItemType Directory -Path "bin"
-}
-docker save homeserver-webapi:arm -o bin/homeserver-webapi.tar
+"Tag WebAPI image"
+docker tag homeserver-webapi:arm 192.168.1.21:5000/homeserver-webapi:latest
 
-"Transfer WebAPI tar file to neph-pi"
-scp bin/homeserver-webapi.tar neph-pi:~/dev
-
-"Transfer WebAPI secrets to neph-pi"
-scp $env:APPDATA\Microsoft\UserSecrets\33c19101-aa4e-4952-8538-07c141d452e8\secrets.json neph-pi:~/dev
-
-"Load WebAPI tar file in docker"
-ssh neph-pi "docker load -i ~/dev/homeserver-webapi.tar"
-
-"Remove old WebAPI image"
-ssh neph-pi "docker rm -f homeserver-webapi"
-
-"Run WebAPI image"
-ssh neph-pi "docker run -d --name homeserver-webapi -p 8081:80 -v /home/neph/dev/secrets.json:/app/secrets.json homeserver-webapi:arm -v /home/neph/dev/logs:/app/logs"
-
-"Check WebAPI status"
-$status = ssh neph-pi "docker inspect -f '{{.State.Status}}' homeserver-webapi"
-if ($status -ne "running") {
-    throw "Container failed to start. Status: $status"
-}
+"Push WebAPI image"
+docker push 192.168.1.21:5000/homeserver-webapi:latest
 
 Set-Location $PSScriptRoot\webpage
 
 "Building webpage image"
 docker buildx build --platform linux/arm64 -t homeserver-webpage:arm --load .
 
-"Exporting webpage image to tar file"
-if (-not(Test-Path "bin"))
-{
-    New-Item -ItemType Directory -Path "bin"
-}
-docker save homeserver-webpage:arm -o bin/homeserver-webpage.tar
+"Tag webpage image"
+docker tag homeserver-webpage:arm 192.168.1.21:5000/homeserver-webpage:latest
 
-"Transfer webpage tar file to neph-pi"
-scp bin/homeserver-webpage.tar neph-pi:~/dev
+"Push webpage image"
+docker push 192.168.1.21:5000/homeserver-webpage:latest
 
-"Load webpage tar file in docker"
-ssh neph-pi "docker load -i ~/dev/homeserver-webpage.tar"
+Set-Location $PSScriptRoot
 
-"Remove old webpage image"
-ssh neph-pi "docker rm -f homeserver-webpage"
+"Transfer docker-compose.yml to neph-pi"
+scp docker-compose.yml neph-pi:~/dev/
 
-"Run webpage image"
-ssh neph-pi "docker run -d --name homeserver-webpage -p 8080:80 homeserver-webpage:arm"
+"Transfer WebAPI secrets to neph-pi"
+scp $env:APPDATA\Microsoft\UserSecrets\33c19101-aa4e-4952-8538-07c141d452e8\secrets.json neph-pi:~/dev
 
-"Check webpage status"
-$status = ssh neph-pi "docker inspect -f '{{.State.Status}}' homeserver-webpage"
-if ($status -ne "running") {
-    throw "Container failed to start. Status: $status"
-}
+"Execute docker compose"
+ssh neph-pi "cd ~/dev/ && docker compose pull"
+ssh neph-pi "cd ~/dev/ && docker compose up -d"
